e92f09ca2a2288c0840d97581804fb51987c9e35
nzvio <7573497@gmail.com>
2021-09-01 02:49:14 +0300

    Update google-entered.auth.page.ts

--- owner/src/app/views/auth/google-entered/google-entered.auth.page.ts
+++ owner/src/app/views/auth/google-entered/google-entered.auth.page.ts
@@ -39,7 +39,7 @@ export class GoogleEnteredAuthPage implements OnInit {
             if (statusCode === 200) {
                 this.router.navigateByUrl("/");                            
             } else if (statusCode === 401) {
-                this.appService.showError(this.words['common']['error-401'][this.currentLang.slug]);	
+                //this.appService.showError(this.words['common']['error-401'][this.currentLang.slug]);	
                 this.router.navigateByUrl("/auth/login");			
             } else {
                 this.appService.showError(this.words['common']['error'][this.currentLang.slug]);				
