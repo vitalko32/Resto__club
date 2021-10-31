33c6fbcc26267d7a37b46157054a4ffe96f997b8
nzvio <7573497@gmail.com>
2021-10-31 16:41:27 +0200

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const corsedUrls = ["https://mgr.restclick.vio.net.ua", "https://owner.restclick.vio.net.ua", "https://restorator.restclick.vio.net.ua", "https://customer.restclick.vio.net.ua"];
+const corsedUrls = [
+	"https://mgr.restclick.vio.net.ua", 
+	"https://owner.restclick.vio.net.ua", 
+	"https://restorator.restclick.vio.net.ua", 
+	"https://customer.restclick.vio.net.ua",
+	"https://amr98.resto-club.com",
+	"https://a.resto-club.com",
+	"https://r.resto-club.com",
+	"https://order.resto-club.com",
+];
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);	
